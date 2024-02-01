82f7d50a181c1288102f6daeca01c11f960f71fd
LarsGJobloop <lars.gunnar@jobloop.no>
2024-02-01 16:52:07 +0100

    add stub functions for handling add task and update display

--- main.js
+++ main.js
@@ -18,5 +18,18 @@ import * as todoList from "./scripts/todoList.js";
 import { createTodoElement } from "./scripts/createTodoElement.js";
 
 // What elements do we need?
+
+/**
+ * Contains the logic for what should happen
+ * when a user presses the submit/add button
+ */
+function handleUserSubmit() {}
+
+/**
+ * Takes a list of todes and renders them to our display
+ *
+ * @param {Todo[]} todoes
+ */
+function updateDisplay(todoes) {}
 // What events do we need to listen for?
 // What should happen when those event occur?
